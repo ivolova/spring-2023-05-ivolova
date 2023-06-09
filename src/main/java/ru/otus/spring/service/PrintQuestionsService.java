413382b3f51295b1d8c0527bb45e4817ff6ee18d
i.volova <ivolova2011@gmail.com>
2023-06-09 22:50:46 +0300

    homework_1 v2

--- src/main/java/ru/otus/spring/service/PrintQuestionsService.java
+++
@@ -1,6 +0,0 @@
-package ru.otus.spring.service;
-
-public interface PrintQuestionsService {
-    void printQuestions();
-
-}
--- src/main/java/ru/otus/spring/service/QuestionService.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.spring.service;
-
-import ru.otus.spring.dto.Question;
-
-import java.util.List;
-
-public interface QuestionService {
-    List<Question> getQuestions();
-}
